42373b577b0f7ec87495b00cfa88e1557236d167
garcialopez <joseadrian_g97@hotmail.com>
2023-11-18 14:38:59 -0600

    Ajustes

--- js/bundle.js
+++
@@ -1,4 +0,0 @@
-import "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/katex.min.js";
-import "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
-import "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js";
-import "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js";
